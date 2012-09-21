99531d41e0250539a28ab2cdc9c38e7916f07c16
Nathan <nathan@nathandumont.com>
2012-09-21 14:07:21 +0100

    Added the basic gd code

--- views/question_page.php
+++ views/question_page.php
@@ -16,6 +16,45 @@
 	<h2 class="hn"><span><?php echo $forum_page['question']; ?> </span></h2>
 </div>
 <div class="main-content main-frm">
+  <label><?php echo $lang_antibot['description']; ?><br />
+                                                <?php
+                                                  // Create the image
+                                                  $code='';
+                                                  $im = imagecreatetruecolor(180, 40);
+                                                  $chr = imagecreatetruecolor(32,32);
+                                                  $chars=imagecreatefrompng("font.png");
+
+                                                  // Create some colors
+                                                  $white = imagecolorallocate($im, 255, 255, 255);
+                                                  $black = imagecolorallocate($im, 0, 0, 0);
+                                                  imagefilledrectangle($im, 0, 0, 180, 40, $white);
+
+                                                  $oldspacing=10;
+                                                  for($i=0;$i<6;$i++)
+                                                  {
+                                                    $num=rand(0,34);
+                                                    $row=($num-fmod($num,8))/8;
+                                                    $col=fmod($num,8);
+                                                    $text = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+                                                    $size=rand(24,32);
+                                                    $spacing=rand(23,25);
+                                                    $ypos=rand(0,8);
+                                                    imagefilledrectangle($chr,0,0,32,32,$white);
+                                                    imagecopyresampled($chr,$chars,0,0,$col*32,$row*32,$size,$size,32,32);
+                                                    imagecolortransparent($chr,$white);
+                                                    imagecopymerge($im,$chr,$oldspacing,$ypos,0,0,32,32,100);
+                                                    $code=$code . $text[$num];
+                                                    $oldspacing=$oldspacing+$spacing;
+                                                  }
+                                                  $code=crypt($code);
+                                                  imagepng($im, "img/avatars/secpic.png");
+                                                  chmod("img/avatars/secpic.png",0666);
+                                                ?>
+
+                                                <img alt='image' src='<?php echo "img/avatars/secpic.png";?>'/><br />
+                                                <input type='text' name='antibot' size='6'/>
+                                                <input type='hidden' value='<?php echo $code;?>' name='code'/>
+                                                <?php echo $lang_antibot['capitals'];?></label>
 	<form id="afocus" class="frm-form" method="post" accept-charset="utf-8" action="<?php echo $forum_page['form_handler']; ?>">
 		<div class="hidden">
 		<?php foreach ($forum_page['hidden_fields'] as $hidden_key => $hidden_value): ?>
